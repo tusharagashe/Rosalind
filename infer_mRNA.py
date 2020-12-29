cc9c9cf8de8b96e59a9093e66ae9422dbc5623cf
Tushar-MAC <tusharagashe1@gmail.com>
2020-12-29 00:54:24 -0800

    infer mRna

---
+++ infer_mRNA.py
@@ -0,0 +1,32 @@
+
+table = {"UUU":"F", "UUC":"F", "UUA":"L", "UUG":"L",
+    "UCU":"S", "UCC":"S", "UCA":"S", "UCG":"S",
+    "UAU":"Y", "UAC":"Y", "UAA":"STOP", "UAG":"STOP",
+    "UGU":"C", "UGC":"C", "UGA":"STOP", "UGG":"W",
+    "CUU":"L", "CUC":"L", "CUA":"L", "CUG":"L",
+    "CCU":"P", "CCC":"P", "CCA":"P", "CCG":"P",
+    "CAU":"H", "CAC":"H", "CAA":"Q", "CAG":"Q",
+    "CGU":"R", "CGC":"R", "CGA":"R", "CGG":"R",
+    "AUU":"I", "AUC":"I", "AUA":"I", "AUG":"M",
+    "ACU":"T", "ACC":"T", "ACA":"T", "ACG":"T",
+    "AAU":"N", "AAC":"N", "AAA":"K", "AAG":"K",
+    "AGU":"S", "AGC":"S", "AGA":"R", "AGG":"R",
+    "GUU":"V", "GUC":"V", "GUA":"V", "GUG":"V",
+    "GCU":"A", "GCC":"A", "GCA":"A", "GCG":"A",
+    "GAU":"D", "GAC":"D", "GAA":"E", "GAG":"E",
+    "GGU":"G", "GGC":"G", "GGA":"G", "GGG":"G"}
+
+file = open("/Users/tusharagashe/Desktop/Workspace/rosalind/rosalind_mrna.txt", 'r')
+s = file.read()
+unique_RNAnum = 3
+
+for protein in s:
+  multiplier = 0
+  for RNA in table: 
+    if table[RNA] == protein: 
+        multiplier += 1
+  if multiplier > 0:
+    unique_RNAnum = unique_RNAnum*multiplier
+
+print unique_RNAnum % 1000000
+
